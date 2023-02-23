00df12af371b11b7e2f070738753d1ce943fd65d
nikhil <nikhilshanmugam.ns@gmail.com>
2023-02-23 14:17:54 +0530

    End of section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+	public class NZWalksDbContext : DbContext
+	{
+		public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+		{
+
+		}
+
+		public DbSet<Region> Regions { get; set; } //Create a table
+
+		public DbSet<Walk> Walks { get; set; } //Create a table
+
+		public DbSet<WalkDifficulty> WalkDifficulty { get; set; } //Its a lookUp table
+	}
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,23 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+	public class Walk
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+
+		public double Length { get; set; }
+
+		public Guid RegionId { get; set; }
+
+		public Guid WalkDifficultyId { get; set; }
+
+		//Navigation Property (Connection of tables in the DB is established during with help of EF core)
+		public Region Region { get; set; }
+
+		public WalkDifficulty WalkDifficulty { get; set; }
+
+	}
+}
+
+//EF Core is the modern DB mapper for .NET core
